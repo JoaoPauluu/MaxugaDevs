fb57fc8c7ad293f0494f72d0ed86dd6098d32569
JoaoPauluu <jpgasperi1@gmail.com>
2022-01-30 13:18:14 -0300

    Added the stop command

--- commands/stop.js
+++ commands/stop.js
@@ -1,5 +1,23 @@
-function command() {
+const Queue = require('../voice/queue');
+const { simpleEmbed } = require('../functions/embeds');
 
+async function command(message, args) {
+    const guildId = message.guildId;
+    const hasQueue = await Queue.hasQueue(guildId);
+
+    if(!hasQueue) {
+        message.reply(simpleEmbed("I'm not currently playing anything!"));
+        return;
+    }
+    try {
+        const queue = await Queue.getQueue(guildId);
+        await queue.connection.destroy();
+        await Queue.deleteQueue(guildId);
+        message.reply(simpleEmbed("Player stopped and queue cleared!"));
+    } catch (e) {
+        console.log(e);
+        message.reply(simpleEmbed("Something went wrong :("));
+    }
 
 }
 
